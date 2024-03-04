fa65be1f88319f0b56df4529625c1096e15b7cd2
Matthew Stobbs <matthew.stobbs@ucalgary.ca>
2024-03-04 16:16:19 -0700

    Fixed port mappings in docker-compose files
    - data-portal, ingest and federation were mapping the external port as
      the container internal port. Those all default to port 3000
      internally.
    - Removed UHN VPN check from pre-build-check.sh, as it's just broken

--- pre-build-check.sh
+++ pre-build-check.sh
@@ -12,10 +12,10 @@ if [[ $* == *-s* ]]; then
     SILENT_MODE=1
 fi
 
-if [[ ! -z "$CANDIG_HOST" ]] && [[ "$SILENT_MODE" != 1 ]]; then
-    printf "Please disable the UHN VPN, as it causes errors with the build process"
-    exit 1
-fi
+#if [[ ! -z "$CANDIG_HOST" ]] && [[ "$SILENT_MODE" != 1 ]]; then
+#    printf "Please disable the UHN VPN, as it causes errors with the build process"
+#    exit 1
+#fi
 
 # Check 2: The value of CANDIG_DOMAIN can be reached
 if [ -z "$CANDIG_DOMAIN" ]; then
